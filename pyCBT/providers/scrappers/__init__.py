1b493d4f04d62e578b07c2d3cc667f8d77f11a7f
cbtradinggroup <41455792+cbtradinggroup@users.noreply.github.com>
2018-08-09 08:36:14 -0400

    Delete __init__.py

--- pyCBT/providers/scrappers/__init__.py
+++
@@ -1,5 +0,0 @@
-# TODO: build common package with modules:
-#       * tables (for parsing tables, units, etc)
-#       * cmdline (for parsing command line)
-#       * decorators (for locale setting, etc)
-#       * output (for processing tables to output streams)
